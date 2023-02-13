9a16f1d11e6577903155678ac11eecf767b6aeb7
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-02-12 22:03:30 -0300

    correção de bug 3

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-const localStorageKey = 'to-do-list-vn'
+const localStorageKey = 'to-do-list-gn'
 
-function newTask ()
+function validateIfExistsNewTask()
+{
+    let values     = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let inputValue = document.getElementById('input-new-task').value
+    let exists     = values.find(x => x.name == inputValue)
+    return !exists ? false : true
+}
+
+function newTask()
 {
     let input = document.getElementById('input-new-task')
+    input.style.border = ''
 
-    //validation
+    // validation
     if(!input.value)
     {
+        input.style.border = '1px solid red'
         alert('Digite algo para inserir em sua lista')
     }
-    else {
-        //puxando o testo do localStorage
+    else if(validateIfExistsNewTask())
+    {
+        alert('Já existe uma task com essa descrição')
+    }
+    else
+    {
+        // increment to localStorage
         let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-        //Capurando os valores do input
         values.push({
             name: input.value
         })
         localStorage.setItem(localStorageKey,JSON.stringify(values))
-        showValues ()
+        showValues()
     }
+    input.value = ''
 }
 
 function showValues()
