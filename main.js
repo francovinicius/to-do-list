b6b63400fa0dd006ba3fe501524e0c7e290a2c28
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-02-12 21:55:30 -0300

    criando função de aparecer a escrita e armazenar

--- main.js
+++ main.js
@@ -14,12 +14,25 @@ function newTask ()
     //increment to localStorage
     else {
         //puxando o testo do localStorage
-        let values = JSON.parse(localStorage.getItem('localStorageKey') || "[]")
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
         //Capurando os valores do input
         values.push({
             name: input.value
         })
         localStorage.setItem(localStorageKey,JSON.stringify(values))
-    
+        showValues ()
     }
 }
+
+function showValues()
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
+    for(let i = 0; i < values.length; i++)
+    {
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
+    }
+}
+
+showValues()
